b5d87b3fe05f2c028543cee3ee5285c28974069b
Kevin Moylan <kmoylan@npr.org>
2012-10-26 15:57:31 -0400

    Adding cron-based retrieval from the API and push protection
    Also don't try to push if the Push API Key is not set
    and don't try to push a story that just came down from the API

--- get_stories.php
+++ get_stories.php
@@ -3,10 +3,61 @@ require_once( 'get_stories_ui.php' );
  
 require_once ('classes/NPRAPIWordpress.php');
 
+
+/**
+register_activation_hook(DS_NPR_PLUGIN_FILE, array ('DS_NPR_API','ds_npr_story_activation'));
+add_action('npr_ds_hourly_cron', array ('DS_NPR_API','ds_npr_story_cron_pull'));
+register_deactivation_hook(DS_NPR_PLUGIN_FILE, array ('DS_NPR_API','ds_npr_story_deactivation'));
+**/
+		
  class DS_NPR_API {
 
 	var $created_message = '';
-	
+
+	public static function ds_npr_story_cron_pull() {
+		// here we should get the list of IDs/full urls that need to be checked hourly
+
+		//because this is run on cron, and may be fired off by an non-admin, we need to load a bunch of stuff
+		require_once (WP_PLUGIN_DIR.'/../../wp-admin/includes/file.php');
+		require_once (WP_PLUGIN_DIR.'/../../wp-admin/includes/media.php');
+		require_once (WP_PLUGIN_DIR.'/../../wp-admin/includes/admin.php');
+		require_once (WP_PLUGIN_DIR.'/../../wp-load.php');
+		require_once (WP_PLUGIN_DIR.'/../../wp-includes/class-wp-error.php');
+		
+		//here we go.
+		$num =  get_option('ds_npr_num');
+
+		for ($i=0; $i<$num; $i++){
+			$api = new NPRAPIWordpress(); 
+			$q = 'ds_npr_query_'.$i;
+			$query_string = get_option($q);
+			error_log('querying for '. $query_string);
+			//if the query string contains the pull url and 'query', just make request from the API
+			if (stristr($query_string, get_option('ds_npr_api_pull_url')) && stristr($query_string,'query')){
+				$api->query_by_url($query_string);
+			}
+			//if the string doesn't contain the base url, try to query using an ID
+			else{
+				$params = array ('id' => $query_string, 'apiKey' => get_option( 'ds_npr_api_key' ));
+        $api->request($params, 'query', get_option( 'ds_npr_api_pull_url' ));
+			}
+			$api->parse();
+      //var_dump($api);
+      try {
+	      if (empty($api->message) || ($api->message->level != 'warning')){
+	      	$story = $api->update_posts_from_stories();
+	      }
+	      else {
+		    	if ( empty($story) ) {
+		          error_log('Not going to save story.  Return from query='. $query_string .', we got an error='.$api->message->id. ' error');
+		      }
+	      }
+      } catch (Exception $e){
+      	error_log('we have an error going in '. __FUNCTION__. ' like this :'. $e);
+      }
+		}
+	}
+   
     function load_page_hook() {
         if ( isset( $_POST ) && isset( $_POST[ 'story_id' ] ) ) {
             $story_id =  $_POST[ 'story_id' ] ;
@@ -19,15 +70,13 @@ require_once ('classes/NPRAPIWordpress.php');
         }
         else if ( isset( $_GET[ 'story_id' ]) && isset( $_GET[ 'create_draft' ]) ) {
             $story_id = $_GET[ 'story_id' ] ;
-            
         }
         
         if ( isset( $story_id ) ) {
 
             // XXX: check that the API key is actually set
-            //$api = new NPR_API_Client( get_option( 'ds_npr_api_key' ) );
             $api = new NPRAPIWordpress(); 
-	          //check to see if we go an ID or a URL
+	          //check to see if we got an ID or a URL
 		        if (is_numeric($story_id)){
 					    if (strlen($story_id) >= 8){
 					      $story_id = $story_id;
@@ -41,34 +90,35 @@ require_once ('classes/NPRAPIWordpress.php');
 						    $story_id = preg_replace('/http\:\/\/[^\s\/]*npr\.org\/([^&\s\<]*storyId\=([0-9]+)).*/', '$2', $story_id);
 						  }
 						}
-            //var_dump($story_id);
             $params = array ('id' => $story_id, 'apiKey' => get_option( 'ds_npr_api_key' ));
             $api->request($params, 'query', get_option( 'ds_npr_api_pull_url' ));
             
             $api->parse();
-            //var_dump($api);
             
-            $story = $api->update_posts_from_stories($publish);
-            if ( ! $story ) {
-                // XXX: handle error
-                return;
+            if (empty($api->message) || ($api->message->level != 'warning')){
+            	$story = $api->update_posts_from_stories($publish);
+            }
+            else {
+	            if ( empty($story) ) {
+	                error_log('Not going to save the return from query for '. $story_id .', we got an error='.$api->message->id. ' error');
+	                return;
+	            }
             }
         }
     }
     function DS_NPR_API() {
         if ( ! is_admin() ) {
-            //add_action( 'the_content', array( &$this, 'embed_audio_clip' ) );
             return;
         }
 
         add_action( 'admin_menu', array( &$this, 'admin_menu' ) );
-        //add_action( 'load-posts_page_get-stories', array( &$this, 'load_page_hook' ) );
         add_action( 'load-posts_page_get-npr-stories', array( 'DS_NPR_API', 'load_page_hook' ) );
     }
 	
     function admin_menu() {
         add_posts_page( 'Get NPR DS Stories', 'Get DS NPR Stories', 'edit_posts', 'get-npr-stories',   'ds_npr_get_stories' );
     }
+    
 }
 
 new DS_NPR_API;
--- settings.php
+++ settings.php
@@ -6,8 +6,22 @@ require_once( 'settings_ui.php' );
 function ds_npr_add_options_page() {
     add_options_page( 'NPR API', 'NPR API', 'manage_options',
                       'ds_npr_api', 'ds_npr_api_options_page' );
+    
+}
+function ds_npr_add_query_page() {
+	$num =  get_option('ds_npr_num');
+	$k = $num;
+  $opt = get_option('ds_npr_query_'.$k);
+  while ($k < NPR_MAX_QUERIES) {
+  	delete_option('ds_npr_query_'.$k);
+  	$k++;
+  	$opt = get_option('ds_npr_query_'.$k);
+  }
+  add_options_page('NPR API Get Multiple', 'NPR API Get Multi', 'manage_options',
+									 'ds_npr_api_get_multi_settings', 'ds_npr_api_get_multi_options_page');
 }
 add_action( 'admin_menu', 'ds_npr_add_options_page' );
+add_action( 'admin_menu', 'ds_npr_add_query_page' );
 
 function ds_npr_settings_init() {
     add_settings_section( 'ds_npr_api_settings', 'NPR API settings', 'ds_npr_api_settings_callback', 'ds_npr_api' );
@@ -24,11 +38,34 @@ function ds_npr_settings_init() {
     add_settings_field( 'ds_npr_api_org_id', 'Org ID', 'ds_npr_api_org_id_callback', 'ds_npr_api', 'ds_npr_api_settings' );
     register_setting( 'ds_npr_api', 'ds_npr_api_org_id' );
 
+    add_settings_section( 'ds_npr_api_get_multi_settings', 'NPR API multiple get settings', 'ds_npr_api_get_multi_settings_callback', 'ds_npr_api_get_multi_settings' );
+
+    add_settings_field( 'ds_npr_num', 'Number of things to get', 'ds_npr_api_num_multi_callback', 'ds_npr_api_get_multi_settings', 'ds_npr_api_get_multi_settings' );
+    register_setting( 'ds_npr_api_get_multi_settings', 'ds_npr_num' );
+		$num =  get_option('ds_npr_num');
+    for($i = 0; $i < $num; $i++){
+    	add_settings_field( 'ds_npr_query_'.$i, 'Query String '.$i, 'ds_npr_api_query_callback', 'ds_npr_api_get_multi_settings', 'ds_npr_api_get_multi_settings', $i );
+    	register_setting( 'ds_npr_api_get_multi_settings', 'ds_npr_query_'.$i );
+    }
+
 }
 add_action( 'admin_init', 'ds_npr_settings_init' );
 
 function ds_npr_api_settings_callback() { }
 
+function ds_npr_api_get_multi_settings_callback() { 
+	 
+}
+
+function ds_npr_api_query_callback($i){
+	$option = get_option('ds_npr_query_'.$i);
+	$name = 'ds_npr_query_'.$i;
+	echo "<input type='text' value='$option' name='$name' style='width: 300px;' />";
+}
+function ds_npr_api_num_multi_callback(){
+	$option = get_option('ds_npr_num');
+	echo "<input type='text' value='$option' name='ds_npr_num' style='width: 30px;' />";
+}
 function ds_npr_api_key_callback() {
     $option = get_option( 'ds_npr_api_key' );
     echo "<input type='text' value='$option' name='ds_npr_api_key' style='width: 300px;' />"; 
