877aea5a2ae714403ac85d764ecf3cd80f72c575
Ryan Nagle <rmnagle@gmail.com>
2016-06-02 15:01:23 -0500

    tests for function in ds-npr-api.php file

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,12 +4,7 @@ $wp_tests_dir = getenv('WP_TESTS_DIR');
 require_once $wp_tests_dir . '/includes/functions.php';
 
 function _manually_load_environment() {
-	// Add your theme
-	switch_theme("your-theme-name");
-
-	// Update array with plugins to include ...
-	$plugins_to_active = array("your-plugin/your-plugin.php");
-
+	$plugins_to_active = array("WP-DS-NPR-API/ds-npr-api.php");
 	update_option('active_plugins', $plugins_to_active);
 }
 tests_add_filter('muplugins_loaded', '_manually_load_environment');
--- tests/test-ds-npr-api.php
+++ tests/test-ds-npr-api.php
@@ -3,27 +3,58 @@
 class Test_DsNprApi extends WP_UnitTestCase {
 
 	function test_ds_npr_story_activation() {
-		$this->markTestIncomplete('This test has not been implemented yet.');
+		ds_npr_story_activation();
+
+		$result = wp_next_scheduled( 'npr_ds_hourly_cron' );
+		$this->assertTrue( ! empty( $result ) );
+
+		$option = get_option( 'ds_npr_num' );
+		$this->assertEquals( $option, 5 );
+
+		$option = get_option( 'ds_npr_api_pull_url' );
+		$this->assertEquals( $option, 'http://api.npr.org' );
 	}
 
 	function test__ds_npr_activate() {
-		$this->markTestIncomplete('This test has not been implemented yet.');
+		$this->markTestSkipped(
+			'Functional test of __ds_npr_activate performed by Test_DsNprApi::test_ds_npr_story_activation');
 	}
 
 	function test_ds_npr_story_deactivation() {
-		$this->markTestIncomplete('This test has not been implemented yet.');
+		ds_npr_story_deactivation();
+
+		$result = wp_next_scheduled( 'npr_ds_hourly_cron' );
+		$this->assertTrue( empty( $result ) );
+
+		$option = get_option( 'ds_npr_num', false );
+		$this->assertFalse( $option );
+
+		$option = get_option( 'ds_npr_api_pull_url', false );
+		$this->assertFalse( $option );
 	}
 
 	function test__ds_npr_deactivate() {
-		$this->markTestIncomplete('This test has not been implemented yet.');
+		$this->markTestSkipped(
+			'Functional test of __ds_npr_deactivate performed by Test_DsNprApi::test_ds_npr_story_deactivation');
 	}
 
 	function test_ds_npr_show_message() {
-		$this->markTestIncomplete('This test has not been implemented yet.');
+		$test_message = 'Test message';
+		ob_start();
+		ds_npr_show_message( $test_message, false );
+		$result = ob_get_clean();
+		$this->assertTrue( (bool) strstr( $result, $test_message ) );
+
+		ob_start();
+		ds_npr_show_message( $test_message, true );
+		$result = ob_get_clean();
+		$this->assertTrue( (bool) strstr( $result, 'class="error"' ) );
 	}
 
 	function test_ds_npr_create_post_type() {
-		$this->markTestIncomplete('This test has not been implemented yet.');
+		ds_npr_create_post_type();
+		$post_types = get_post_types();
+		$this->assertTrue( in_array( NPR_POST_TYPE, $post_types ) );
 	}
 
 }
