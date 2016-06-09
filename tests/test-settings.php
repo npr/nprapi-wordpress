04815986c76c97b5d098bad8f26b7979b444fc5b
Ryan Nagle <rmnagle@gmail.com>
2016-06-09 14:33:00 -0500

    unit tests for functions in settings.php

--- tests/test-settings.php
+++ tests/test-settings.php
@@ -15,71 +15,112 @@ class Test_Settings extends WP_UnitTestCase {
 	}
 
 	function test_ds_npr_api_settings_callback() {
-		$this->markTestIncomplete('This test has not been implemented yet.');
+		# TODO: do we need ds_npr_api_settings_callback if it does nothing?
+		$ret = ds_npr_api_settings_callback();
+		$this->assertTrue( is_null( $ret ) );
 	}
 
 	function test_cron_add_ds_interval() {
-		$this->markTestIncomplete('This test has not been implemented yet.');
+		$test_schedules = array();
+		$ret = cron_add_ds_interval( $test_schedules );
+		$this->assertTrue( isset( $ret['ds_interval'] ) );
 	}
 
-	function test_ds_npr_api_multi_settings_callback() {
-		$this->markTestIncomplete('This test has not been implemented yet.');
+	function test_ds_npr_api_get_multi_settings_callback() {
+		ds_npr_api_get_multi_settings_callback();
+		$schedules = wp_get_schedule( 'npr_ds_hourly_cron' );
+		$this->assertTrue( ! empty( $schedules ) );
 	}
 
 	function test_dp_npr_query_run_multi_callback() {
-		$this->markTestIncomplete('This test has not been implemented yet.');
+		# Simple test of output -- expect an input with id dp_npr_query_run_multi
+		$this->expectOutputRegex('/<input id\=\'dp_npr_query_run_multi\'.*/');
+		dp_npr_query_run_multi_callback();
 	}
 
 	function test_ds_npr_query_multi_cron_interval_callback() {
-		$this->markTestIncomplete('This test has not been implemented yet.');
+		# Simple test of outut -- should output an input element with a label matching below
+		$this->expectOutputRegex('/<input.*How often, in minutes, should the Get Multi function run\?  \(default \= 60\)/');
+		dp_npr_query_multi_cron_interval_callback();
 	}
 
 	function test_ds_npr_api_query_publish_callback() {
-		$this->markTestIncomplete('This test has not been implemented yet.');
+		# Output test -- expect div element plus text shown below, value of
+		# 'ds_npr_api_query_publish' . $i option should be included in the output.
+		$i = 'test';
+		update_option( 'ds_npr_query_publish_' . $i, 'test' );
+		$this->expectOutputRegex('/<div>Publish or Draft the returns from Query.*test.*/');
+		ds_npr_api_query_publish_callback($i);
 	}
 
 	function test_ds_npr_api_query_callback() {
-		$this->markTestIncomplete('This test has not been implemented yet.');
+		# Output test -- make sure passed parameter is used in output of input element
+		$i = 'test';
+		update_option( 'ds_npr_query_' . $i, 'test' );
+		$this->expectOutputRegex(
+			'/<input type\=\'text\' value\=\'test\' name\=\'ds_npr_query_test\' style\=\'width: 300px\;\' \/>/');
+		ds_npr_api_query_callback($i);
 	}
 
 	function test_ds_npr_api_num_multi_callback() {
-		$this->markTestIncomplete('This test has not been implemented yet.');
+		update_option( 'ds_npr_num', 'test' );
+		$this->expectOutputRegex('/<input type\=\'text\' value=\'test\'.*/');
+		ds_npr_api_num_multi_callback();
 	}
 
 	function test_ds_npr_api_key_callback() {
-		$this->markTestIncomplete('This test has not been implemented yet.');
+		update_option( 'ds_npr_api_key', 'test' );
+		$this->expectOutputRegex('/<input type\=\'text\' value=\'test\'.*/');
+		ds_npr_api_key_callback();
 	}
 
 	function test_ds_npr_api_pull_url_callback() {
-		$this->markTestIncomplete('This test has not been implemented yet.');
+		update_option( 'ds_npr_api_pull_url', 'test' );
+		$this->expectOutputRegex('/<input type\=\'text\' value=\'test\'.*/');
+		ds_npr_api_pull_url_callback();
 	}
 
 	function test_ds_npr_api_push_url_callback() {
-		$this->markTestIncomplete('This test has not been implemented yet.');
+		update_option( 'ds_npr_api_push_url', 'test' );
+		$this->expectOutputRegex('/<input type\=\'text\' value=\'test\'.*/');
+		ds_npr_api_push_url_callback();
 	}
 
 	function test_ds_npr_api_org_id_callback() {
-		$this->markTestIncomplete('This test has not been implemented yet.');
+		update_option( 'ds_npr_api_org_id', 'test' );
+		$this->expectOutputRegex('/<input type\=\'text\' value=\'test\'.*/');
+		ds_npr_api_org_id_callback();
 	}
 
 	function test_ds_npr_pull_post_type_callback() {
-		$this->markTestIncomplete('This test has not been implemented yet.');
+		$field_name = 'ds_npr_pull_post_type';
+		$this->expectOutputRegex('/<div><select id\=' . $field_name . '.*/');
+		ds_npr_pull_post_type_callback();
 	}
 
 	function test_ds_npr_push_post_type_callback() {
-		$this->markTestIncomplete('This test has not been implemented yet.');
+		$field_name = 'ds_npr_push_post_type';
+		$this->expectOutputRegex('/<div><select id\=' . $field_name . '.*/');
+		ds_npr_push_post_type_callback();
 	}
 
 	function test_ds_npr_push_story_permissions_callback() {
-		$this->markTestIncomplete('This test has not been implemented yet.');
+		# Expect an error message since we are not actually contacting the API for permissions groups
+		$this->expectOutputRegex('/You have no Permission Groups defined with the NPR API/');
+		ds_npr_push_story_permissions_callback();
 	}
 
 	function test_ds_npr_show_post_types_select() {
-		$this->markTestIncomplete('This test has not been implemented yet.');
+		$field_name = 'test_field';
+		update_option( $field_name, 'test_value' );
+		$this->expectOutputRegex('/<div><select id\=' . $field_name . '.*/');
+		ds_npr_show_post_types_select( $field_name, array() );
 	}
 
 	function test_ds_npr_show_perms_select() {
-		$this->markTestIncomplete('This test has not been implemented yet.');
+		$field_name = 'test_field';
+		$this->expectOutputRegex('/<div><select id\=' . $field_name . '.*/');
+		ds_npr_show_perms_select( $field_name, array() );
 	}
 
 }
